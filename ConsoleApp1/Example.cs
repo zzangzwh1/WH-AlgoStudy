943b9f1ea0a54bb4ce4e4405fb7b21878168c7ab
Wonhyuk Cho <wcho2@nait.ca>
2024-04-19 18:49:21 -0600

    Using HashSet Intesection

---
+++ ConsoleApp1/Example.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Example
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            HashSet<int> uniqueNumbers = new HashSet<int>();
+            HashSet<int> uniqueNumberSecond = new HashSet<int>();
+
+            var returnDic = new Dictionary<int, int>();
+            foreach (var num in uniqueNumbers)
+            {
+                if (!returnDic.ContainsKey(num))
+                {
+                    returnDic.Add(num,1);
+                }
+
+                else
+                {
+                    returnDic[num]++;
+                }
+            }
+            if(returnDic.Values.Count >1)
+            {
+                return returnDic.Keys.ToArray();
+            }
+
+            return new int[0];
+        }
+    }
+}
